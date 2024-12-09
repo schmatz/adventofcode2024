4a12387aad96c48fb9b8d39975f753162f06785e
Michael Schmatz <michaelschmatz@gmail.com>
2024-12-09 06:49:05 +0000

    First problem done

--- 9/day9.py
+++ 9/day9.py
@@ -1,8 +1,10 @@
 from pathlib import Path 
 
-input_text = (Path(__file__).parent / "test1.txt").read_text()
+input_text = (Path(__file__).parent / "input.txt").read_text()
 
 dense_disk_map = [int(char) for char in input_text]
+
+FREE_SPACE_MARKER = -1
 
 expanded_disk_map = []
 current_id = 0
@@ -12,35 +14,30 @@ for i, num in enumerate(dense_disk_map):
     is_file_marker = i % 2 == 0
     if is_file_marker:
         # Can't do this because IDs won't be preserved
-        expanded_disk_map.extend([str(current_id)] * num)
+        expanded_disk_map.extend([current_id] * num)
         current_id += 1
     else:
         # It's free space
-        expanded_disk_map.extend(['.'] * num)
-
-print(expanded_disk_map)
+        expanded_disk_map.extend([FREE_SPACE_MARKER] * num)
 
 rightmost_file_index = len(expanded_disk_map) - 1
 for i in range(len(expanded_disk_map)):
-    if expanded_disk_map[i] != ".":
+    if expanded_disk_map[i] != FREE_SPACE_MARKER:
         continue
 
-    if expanded_disk_map[rightmost_file_index] == '.':
+    if expanded_disk_map[rightmost_file_index] == FREE_SPACE_MARKER:
         for j in reversed(range(i+1, rightmost_file_index + 1)):
-            if expanded_disk_map[j] != '.':
+            if expanded_disk_map[j] != FREE_SPACE_MARKER:
                 rightmost_file_index = j
                 break
         else:
-            print("Ran out of files to compact")
             break
     
     expanded_disk_map[i], expanded_disk_map[rightmost_file_index] = expanded_disk_map[rightmost_file_index], expanded_disk_map[i]
 
-print(expanded_disk_map)
-
 checksum = 0
 for i in range(len(expanded_disk_map)):
-    if expanded_disk_map[i] == '.':
+    if expanded_disk_map[i] == FREE_SPACE_MARKER:
         continue
     checksum += i * int(expanded_disk_map[i])
 print(checksum)
