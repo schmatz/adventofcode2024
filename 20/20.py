b606209659e464e7cc9e9ac700aa1956a2781607
Michael Schmatz <michaelschmatz@gmail.com>
2024-12-20 07:51:19 +0000

    Part 2

--- 20/20.py
+++ 20/20.py
@@ -38,7 +38,7 @@ INFINITY_VALUE = 1_000_000_000_000_000_000
 
 DIRECTIONS = [(0,1), (0, -1), (1, 0), (-1, 0)]
 
-def simple_dijkstras_algorithm(grid: Grid) -> int:
+def simple_dijkstras_algorithm(grid: Grid) -> dict[Coordinate, int]:
     distances: dict[Coordinate, int] = {}
 
     for y, yline in enumerate(grid):
@@ -49,7 +49,6 @@ def simple_dijkstras_algorithm(grid: Grid) -> int:
                 distances[(x,y)] = INFINITY_VALUE
 
     start_coord = find_char_in_grid('S', grid)
-    end_coord = find_char_in_grid('E', grid)
 
     distances[start_coord] = 0
     pq: list[tuple[int, Coordinate]] = [(0, start_coord)]
@@ -76,30 +75,65 @@ def simple_dijkstras_algorithm(grid: Grid) -> int:
                 distances[next_coord] = tentative_distance
                 heappush(pq, (tentative_distance, next_coord))
 
-    return distances[end_coord]
+    return distances
 
 
 #print(simple_dijkstras_algorithm(input_grid))
 
 input_grid = parse_input_from_file("input.txt")
 
-original_completion_time = simple_dijkstras_algorithm(input_grid)
+original_dijkstras_distances = simple_dijkstras_algorithm(input_grid)
 
-cheat_completion_times: dict[Coordinate, int] = {}
-# Remove each wall and recalculate
+end_coord = find_char_in_grid('E', input_grid)
+original_distance_to_end = original_dijkstras_distances[end_coord]
+
+
+# Rather than reprocessing the Dijkstra's, just explore all reachable positions with N turns and then resume the algorithm
+
+BASE_DIRECTIONS = base_directions = [(0,1), (0,-1), (1, 0), (-1, 0)]
+
+def get_end_pos(start: Coordinate, moves: tuple[Coordinate, ...]) -> Coordinate:
+    x = start[0]
+    y = start[1]
+    for combo in moves:
+        x += combo[0]
+        y += combo[1]
+    
+    return (x,y)
+
+MAX_CHEAT_DISTANCE = 20
+cheat_completion_times: dict[tuple[Coordinate, Coordinate], int] = {}
+# TODO: Need to account for early reaching of goal
 for y, yline in enumerate(input_grid):
-    print("processing", y, "yline")
+    print("Processing line", y)
     for x, xchar in enumerate(yline):
         if xchar == '#':
-            old_val = input_grid[y][x]
-            input_grid[y][x] = '.'
-            completion_time = simple_dijkstras_algorithm(input_grid)
-            cheat_completion_times[(x,y)] = completion_time
-            #if y == 7 and x == 6:
-            #    print("Cheating completion time", completion_time, "saves", original_completion_time - completion_time)
-            input_grid[y][x] = old_val
+            continue
+        start_coord = (x,y)
+        original_cost = original_distance_to_end - original_dijkstras_distances[start_coord]
+        for finish_coord in original_dijkstras_distances.keys():
+            manhattan_distance = abs(start_coord[0] - finish_coord[0]) + abs(start_coord[1] - finish_coord[1])
+            
+            if manhattan_distance > MAX_CHEAT_DISTANCE:
+                continue
 
-grouped_savings = Counter([original_completion_time - completion_time for completion_time in cheat_completion_times.values()])
+            if start_coord == finish_coord:
+                continue
+
+            finish_char = input_grid[finish_coord[1]][finish_coord[0]]
+            assert finish_char != '#'
+
+            new_cost = original_distance_to_end - original_dijkstras_distances[finish_coord] + manhattan_distance
+            # We now incur a movement of cheat_distance
+            time_savings = original_cost - new_cost
+            if time_savings <= 0:
+                continue # We don't save any time by doing the cheat
+            
+            cheat_completion_times[(start_coord, finish_coord)] = time_savings
+            
+
+
+grouped_savings = Counter(cheat_completion_times.values())
 del grouped_savings[0]
 print(sorted(grouped_savings.items()))
-print(sum([y for x, y in grouped_savings.items() if x >= 100]))
+print(sum([num_cheats for amount_savings, num_cheats in grouped_savings.items() if amount_savings >= 100]))
